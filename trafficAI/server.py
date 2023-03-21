5e93884c97cb30985bc1c2b81aca5a0ef1b50c6d
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-21 18:07:29 -0300

    new

---
+++ trafficAI/server.py
@@ -0,0 +1,68 @@
+from concurrent import futures
+import time
+import os
+import grpc
+import api_pb2_grpc
+import api_pb2
+import numpy as np
+import cv2
+
+class ApiServicer(api_pb2_grpc.GreeterServicer):
+    
+    def SayHello(self, request, context):
+        
+        print("Request {}".format(request))
+        
+        reply = api_pb2.HelloReply()
+        reply.message = f"{request.name}"
+        
+        return reply
+    
+    def SendImage(self, request, context):
+        print("Request: {} bytes".format(len(request.image)))
+        
+        img_array = np.frombuffer(request.image, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        cv2.imshow('Imagem Recebida', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        reply = api_pb2.ImageReply()
+        reply.message = "Enviado"
+        
+        return reply
+        
+    def SendVideo(self, request, context):
+        print("Request: {} bytes".format(len(request.video)))
+
+        with open("video_temp.mp4", "wb") as f:
+            f.write(request.video)
+
+        video = cv2.VideoCapture("video_temp.mp4")
+        while True:
+            ret, frame = video.read()
+            if not ret:
+                break
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+        cv2.destroyAllWindows()
+        video.release()
+
+        reply = api_pb2.VideoReply()
+        reply.message = "Enviado"
+
+        return reply
+    
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), options=[ 
+        ('grpc.max_receive_message_length', 104857600), 
+        ('grpc.max_send_message_length', 104857600)
+    ])
+    api_pb2_grpc.add_GreeterServicer_to_server(ApiServicer(), server)
+    server.add_insecure_port("localhost:50051")
+    server.start()
+    server.wait_for_termination()
+    
+if __name__ == "__main__":
+    serve()
---
+++ trafficAI/tests/test.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.append("..")
+
+import api_pb2_grpc
+import api_pb2
+import time
+import grpc
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051', options=[
+        ('grpc.max_receive_message_length', 104857600),
+        ('grpc.max_send_message_length', 104857600)
+    ]) as channel:
+        stub = api_pb2_grpc.GreeterStub(channel)
+        print("1. Hello - enviar texto")
+        print("2. Imagem - enviar imagem")
+        print("3. Video - enviar video")
+        rpc_call = input("Qual voce quer testar: ")
+        
+        if rpc_call == "1":
+            request = api_pb2.HelloRequest(name = "test")
+            reply = stub.SayHello(request)
+            print("Response {}".format(reply))
+            
+        elif rpc_call == "2":
+            with open("img/car.jpg", "rb") as f:
+                image_bytes = f.read()
+            request = api_pb2.ImageRequest(image=image_bytes)
+            reply = stub.SendImage(request)
+            print("Response {}".format(reply))
+            
+        elif rpc_call == "3":
+            with open("video/car.MOV", "rb") as f:
+                video_bytes = f.read()
+            request = api_pb2.VideoRequest(video=video_bytes)
+            reply = stub.SendVideo(request)
+            print("Response {}".format(reply))
+            
+if __name__ == "__main__":
+    run()
