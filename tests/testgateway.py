6d45cbf4bb571a0b08c061906088283bb16c60bf
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-24 15:00:24 +0000

    new

--- tests/testgateway.py
+++ tests/testgateway.py
@@ -1,7 +1,10 @@
 import requests
 import json
 
-def trafficAIHello(tarefa):
+def trafficAISendimg():
     with open("video/car.MOV", "rb") as f:
                 video_bytes = f.read()
     requests.post("http://localhost:3000/image", data=json.dumps(video_bytes))
+    
+if __name__ == '__main__':
+    trafficAISendimg()
