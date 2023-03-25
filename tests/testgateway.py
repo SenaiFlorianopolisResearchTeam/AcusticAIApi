2a905e3591b96bb13e5b6f10d8b6e2e4dcfe5fdb
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-25 13:34:49 -0300

    new

--- tests/testgateway.py
+++ tests/testgateway.py
@@ -1,10 +1,12 @@
 import requests
-import json
+import base64
 
 def trafficAISendimg():
     with open("video/car.MOV", "rb") as f:
-                video_bytes = f.read()
-    requests.post("http://localhost:3000/image", data=json.dumps(video_bytes))
+        video_bytes = f.read()
+    payload = {"image": base64.b64encode(video_bytes).decode('utf-8')}  # encode em base64
+    headers = {'Content-type': 'application/json'}
+    requests.post("http://localhost:3000/image", json=payload, headers=headers)
     
 if __name__ == '__main__':
     trafficAISendimg()
