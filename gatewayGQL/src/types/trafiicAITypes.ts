224486ca97641485ab0db3da113c9b9053864841
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-31 14:58:02 +0000

    new

--- gatewayGQL/src/types/trafiicAITypes.ts
+++ gatewayGQL/src/types/trafiicAITypes.ts
@@ -1,42 +1,26 @@
 const typeDefTrafficAI = `
-    type HelloRequest {
-        name: String
-    }
-    
     type HelloReply {
         message: String
     }
-    
-    type ImageRequest {
-        image: String
-    }
-    
+        
     type ImageReply {
         message: String
     }
-    
-    type VideoRequest {
-        video: String
-    }
-    
+        
     type VideoReply {
         message: String
-    }
-    
-    type GreeterSayHelloResponse {
-        greeting: HelloReply
-    }
-    
-    type GreeterSendImageResponse {
-        status: ImageReply
-    }
-    
-    type GreeterSendVideoResponse {
-        status: VideoReply
     }
 
     type Query {
         sayHello(name: String): HelloReply
+    }
+
+    type Query {
+        predictImage(image: String): ImageReply
+    }
+
+    type Query {
+        predictVideo(video: String): VideoReply
     }
 `;
 
