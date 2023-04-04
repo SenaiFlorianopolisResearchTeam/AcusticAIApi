04c7dd5518fb746189d5bb99a4bb0ee5f578e518
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-04 14:39:45 +0000

    nwe

--- gatewayGQL/src/resolvers/userRes.ts
+++ gatewayGQL/src/resolvers/userRes.ts
@@ -1,6 +1,7 @@
 import connect from '../tools/database';
 import User from '../models/User';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const resolverUser = {
   Mutation: {
@@ -17,6 +18,24 @@ const resolverUser = {
       await User.create({username: userVar, email: emailVar, password: hashedPassword})
 
       return { message: "user created"}
+    },
+    logUser: async (root:any, args:any, context:any) => {
+
+      const email = args.email
+      const password = args.password
+
+      const JWT = String(process.env.JWT_SECRET)
+
+      await connect()
+
+      const user = await User.findOne({ email });
+
+      if (user && bcrypt.compareSync(password, user.password)) {
+          const token = jwt.sign({ userId: user._id }, JWT, { expiresIn: '1h' });
+          return { token, user };
+      } else {
+          return { error: 'Credenciais inválidas' };
+      }
     },
   },
 };
--- gatewayGQL/src/types/userTypes.ts
+++ gatewayGQL/src/types/userTypes.ts
@@ -3,8 +3,14 @@ const typeDefTrafficAI = `
     message: String
   }
 
+  type logUserReply {
+    token: String
+    message: String
+  }
+
   type Mutation {
     createUser(username: String, email: String, password: String): CreatUserReply!
+    logUser(email: String, password: String): CreatUserReply!
   }
 `;
 
