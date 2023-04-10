f1fcc7b5fedfd24c7174cd75afdba63753364737
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-09 21:28:42 -0300

    new

--- gateway/src/resolvers/trafficAI.ts
+++ gateway/src/resolvers/trafficAI.ts
@@ -1,10 +1,18 @@
+import { blob } from "stream/consumers";
 import hello from "../func/hello"
+import sendImage from "../func/image";
 
 export const trafficAI = {
   Mutation: {
     sayHello: async (root:any, args:any, context:any) => {
-        const response = await hello(args.name);
-        return { message: response.message };
+      const response = await hello(args.name);
+      return { message: response.message };
+    },
+    predictImage: async (root:any, args:any, context:any) => {
+      const response = await sendImage(args.image);
+      
+      const message = "foi"
+      return { message: message };
     },
   }
 };
