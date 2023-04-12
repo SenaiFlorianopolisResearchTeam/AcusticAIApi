46c90d5f0fc63557a00fc877df27a6221999105c
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-11 21:14:09 -0300

    new

--- tests/testtrafficAI.py
+++ tests/testtrafficAI.py
@@ -6,9 +6,15 @@ import api_pb2 as api_pb2
 import time
 import grpc
 
+with open('./cert/server.key', 'rb') as f:
+    client_key = f.read()
+with open('./cert/server.crt', 'rb') as f:
+    client_cert = f.read()
+    
+creds = grpc.ssl_channel_credentials(root_certificates=None, private_key=client_key, certificate_chain=client_cert)
 
 def run():
-    with grpc.insecure_channel('localhost:8070', options=[
+    with grpc.secure_channel('[::]:8070', creds, options=[
         ('grpc.max_receive_message_length', 104857600),
         ('grpc.max_send_message_length', 104857600)
     ]) as channel:
@@ -59,28 +65,7 @@ def image():
         reply = stub.sendImage(request)
         print("Response {}".format(reply))
         
-def video():
-    with grpc.insecure_channel('localhost:8070', options=[
-        ('grpc.max_receive_message_length', 104857600),
-        ('grpc.max_send_message_length', 104857600)
-    ]) as channel:
-        stub = api_pb2_grpc.GreeterStub(channel)
-        with open("video/car.MOV", "rb") as f:
-                video_bytes = f.read()
-        request = api_pb2.VideoRequest(video=video_bytes)
-        reply = stub.sendVideo(request)
-        print("Response {}".format(reply))
-            
 if __name__ == "__main__":
     print(sys.argv)
     if sys.argv == ['testtrafficAI.py']:
         run()
-        
-    if sys.argv == ['testtrafficAI.py', '-1']:
-        hello()
-        
-    if sys.argv == ['testtrafficAI.py', '-2']:
-        image()
-        
-    if sys.argv == ['testtrafficAI.py', '-3']:
-        video()
