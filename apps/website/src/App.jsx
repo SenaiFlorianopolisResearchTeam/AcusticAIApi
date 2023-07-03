4eba90b724ffb06cabd60c07ff96bbde3c5d1684
fullzer4 <gabrielpelizzaro@gmail.com>
2023-07-03 05:41:40 -0300

    test

--- apps/website/src/App.jsx
+++ apps/website/src/App.jsx
@@ -1,9 +1,11 @@
 import WebcamC from "./components/Webcam"
+import "./scss/index.scss"
 
 const App = () => {
 
   return (
     <>
+      <h1>🥳 Teste modelo webcam 🥳</h1>
       <WebcamC/>
     </>
   )
