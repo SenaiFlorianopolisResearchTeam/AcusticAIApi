31be549d502be78bd52e303ae9caa501fa851956
fullzer4 <gabrielpelizzaro@gmail.com>
2023-07-03 05:49:12 -0300

    test

--- apps/website/src/components/Webcam.jsx
+++ apps/website/src/components/Webcam.jsx
@@ -4,38 +4,9 @@ import '../scss/webcam.scss';
 
 const WebcamC = () => {
   const webcamRef = useRef(null);
-  const [multipleCameras, setMultipleCameras] = useState(false);
-  const [facingMode, setFacingMode] = useState('user');
-
-  useEffect(() => {
-    const getMediaDevices = async () => {
-      try {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const videoDevices = devices.filter(device => device.kind === 'videoinput');
-        setMultipleCameras(videoDevices.length > 1);
-      } catch (error) {
-        console.error('Erro ao obter dispositivos de mídia:', error);
-      }
-    };
-
-    getMediaDevices();
-  }, []);
-
-  const switchCamera = () => {
-    setFacingMode(prevMode => (prevMode === 'user' ? 'environment' : 'user'));
-  };
-
-  const videoConstraints = {
-    facingMode: facingMode === 'user' ? 'user' : { exact: 'environment' },
-  };
 
   return (
     <div className="webcam-container">
-      {multipleCameras && (
-        <button onClick={switchCamera}>
-          Alternar câmera
-        </button>
-      )}
       <Webcam
         audio={false}
         ref={webcamRef}
