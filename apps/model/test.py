f3875b4f47abfe841c9206e2acc3e4c88d1836a0
fullzer4 <gabrielpelizzaro@gmail.com>
2023-07-03 04:54:27 -0300

    new tests

--- apps/model/test.py
+++ apps/model/test.py
@@ -8,21 +8,23 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-    results = model.track(frame, persist=True)
-    print(results[0].probs)
-    #classes  = results[0].probs.cpu().numpy().astype(float)
+    results = model.track(frame, save_conf=True, tracker="botsort.yaml")  # ou 'bytetrack.yaml')
+    labels = results[0].names
+    classes = results[0].boxes.cls.cpu().numpy().astype(int)
     boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
     ids = results[0].boxes.id.cpu().numpy().astype(int)
-    for box, id in zip(boxes, ids):
-        cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+    for box, id, class_index in zip(boxes, ids, classes):
+        class_label = labels[class_index]
+        
+        cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 1)
         cv2.putText(
             frame,
-            f"Id {id}",
-            (box[0], box[1]),
+            f"Class: {class_label}",
+            (box[0], box[1] - 10), \
             cv2.FONT_HERSHEY_SIMPLEX,
             0.5,
             (0, 0, 255),
-            2,
+            1,
         )
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
