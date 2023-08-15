5868a443abb97ed0d57e4ca7672b3fcbd1ca1a4f
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-15 09:37:20 -0300

    cardSession

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,15 @@
-const Home = () => {
+import CardSession from "@/components/cardSession"
+import { NextComponentType } from "next"
+
+const Home: NextComponentType = () => {
   return (
     <main>
-      
+      <div>
+        <CardSession id="1" name="Sessão 1" data={[1,2,3,4,5,6]}/>
+      </div>
+      <div>
+
+      </div>
     </main>
   )
 }
---
+++ frontend/src/components/cardSession.tsx
@@ -0,0 +1,66 @@
+import Style from "@/scss/components/cardSession.module.scss"
+import { NextComponentType } from "next";
+
+interface Props { 
+    readonly id: string;
+    readonly name: string;
+    readonly data: Data<number>;
+}
+
+type Data<T> = [
+    horas: T,
+    videos: T,
+    veiculos: T,
+    carros: T,
+    onibus: T,
+    moto: T,
+]
+
+const CardSession = (props: Props) => {
+
+    return (
+        <div className={Style.cardSessionContainer}>
+            <div className={Style.graphContainer}>
+                <div className={Style.session}>
+                    <p className={Style.sessionTitle}>{props.name}</p>
+                    <p className={Style.graphText}>Total de veiculos reconhecidos</p>
+                </div>
+                <div className={Style.graph}>
+                    {/* grafico */}
+                </div>
+            </div>
+            <div className={Style.dataContainer}>
+                <div className={Style.data}>
+                    <p className={Style.dataTitle}>horas</p>
+                    <p className={Style.dataNumber}>{props.data[0]}</p>
+                </div>
+                <div className={Style.data}>
+                    <p className={Style.dataTitle}>videos</p>
+                    <p className={Style.dataNumber}>{props.data[1]}</p>
+                </div>
+                <div className={Style.data}>
+                    <p className={Style.dataTitle}>veiculos</p>
+                    <p className={Style.dataNumber}>{props.data[2]}</p>
+                </div>
+                <div className={Style.data}>
+                    <p className={Style.dataTitle}>carros</p>
+                    <p className={Style.dataNumber}>{props.data[3]}</p>
+                </div>
+                <div className={Style.data}>
+                    <p className={Style.dataTitle}>onibus</p>
+                    <p className={Style.dataNumber}>{props.data[4]}</p>
+                </div>
+                <div className={Style.data}>
+                    <p className={Style.dataTitle}>moto</p>
+                    <p className={Style.dataNumber}>{props.data[5]}</p>
+                </div>
+                <div>
+                    <button>deletar</button>
+                    {/* botar imagem */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CardSession;
