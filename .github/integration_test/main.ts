7b2c9ccf66ab01f5ae0361c4de94dac349296bbd
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-23 09:39:16 -0300

    integration test

--- .github/integration_test/main.ts
+++ .github/integration_test/main.ts
@@ -1,2 +1,22 @@
-import {} from "puppeteer"
-import cheerio from "cheerio"
+import puppeteer from "puppeteer"
+import * as cheerio from "cheerio"
+
+const test = async () => {
+
+    const browser = await puppeteer.launch()
+
+    const page = await browser.newPage()
+
+    await page.goto(`http://localhost:3000/`)
+
+    const htmlC = await page.content()
+
+    await page.screenshot({path: 'test.png'});
+
+    await browser.close()
+
+    const $ = cheerio.load(htmlC)
+    
+}
+
+test()
