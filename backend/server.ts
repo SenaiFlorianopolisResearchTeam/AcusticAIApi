beab85598470e602eedff98842dd15bd5698e57d
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-24 12:28:22 -0300

    jwt setup

--- backend/server.ts
+++ backend/server.ts
@@ -3,12 +3,22 @@ import AutoLoad from '@fastify/autoload';
 import Swagger from '@fastify/swagger';
 import SwaggerUI from '@fastify/swagger-ui';
 import fastifyCors from '@fastify/cors';
+import fastifyJWT from '@fastify/jwt';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export function build () {
+
+  const key: any = process.env.jwt_key;
+
   const server = Fastify();
 
   server.register(Swagger);
   server.register(SwaggerUI);
+  server.register(fastifyJWT, {
+    secret: key
+  });
 
   server.register(fastifyCors, {
     origin: '*'
