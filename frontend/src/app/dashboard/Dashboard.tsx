db152309c41ee8910b875eb88a242da687cc9944
fullzer4 <gabrielpelizzaro@gmail.com>
2023-09-02 15:47:35 -0300

    fix dash layout

--- frontend/src/app/dashboard/Dashboard.tsx
+++ frontend/src/app/dashboard/Dashboard.tsx
@@ -1,18 +1,25 @@
+"use client"
+
 import Navbar from "../../components/navbar"
 import CardSession from "../../components/cardSession"
 import { NextPage } from "next"
 import Styles from "../../scss/dashboard.module.scss"
+import CreateSession from "@/components/createSession"
 
 const Dashboard: NextPage = () => {
-  return (
-    <main className={Styles.dashboard}>
-      <Navbar page="dashboard"/>
-      <div className={Styles.cards}>
-        <p>Create your firts AI traffic session</p>
-        <button>Create</button>
-      </div>
-    </main>
-  )
+
+    return (
+        <main className={Styles.dashboard}>
+            <Navbar page="dashboard" />
+            <div className={Styles.cards}>
+                <div className={Styles.noneCard}>
+                    <p>Create your firts AI traffic session</p>
+                    <button>Create</button>
+                </div>
+            </div>
+            <CreateSession/>
+        </main>
+    )
 }
 
 export default Dashboard
