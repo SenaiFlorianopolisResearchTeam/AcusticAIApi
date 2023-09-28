ba01d342d9eca71cc27d2d97ff5147a21e67f89b
fullzer4 <gabrielpelizzaro@gmail.com>
2023-09-28 07:35:59 -0300

    new

---
+++ ai/model/AcustticAI-4/README.dataset.txt
@@ -0,0 +1,6 @@
+# AcustticAI > 2023-09-28 1:57am
+https://universe.roboflow.com/senai-qb205/acustticai
+
+Provided by a Roboflow user
+License: MIT
+
--- ai/model/train.py
+++ ai/model/train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO('yolov8n.pt')
-    model.train(data='./model/AcustticAI-3/data.yaml', epochs=100, imgsz=[640,384])
+    model.train(data='./AcustticAI-4/data.yaml', epochs=500, imgsz=[640,384], device='cpu')
