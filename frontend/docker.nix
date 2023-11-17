695c27c9cf4c6cf82bb1ddd1fb6c343a0c40a270
fullzer4 <gabrielpelizzaro@gmail.com>
2023-11-17 02:20:10 -0300

    fixes

--- frontend/docker.nix
+++ frontend/docker.nix
@@ -2,15 +2,20 @@
 , pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
 }:
 
-
 pkgs.dockerTools.buildImage {
-
   name = "hello-docker";
-
+  
   config = {
-
     Cmd = [ "${pkgsLinux.hello}/bin/hello" ];
 
-  };
+    WorkingDir = "/app";
+    Copy = [ "." ];
 
+    Run = ''
+      ${pkgs.yarn}/bin/yarn install &&
+
+      ${pkgs.yarn}/bin/yarn build
+    '';
+
+  };
 }
