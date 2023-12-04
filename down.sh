633762a1cd1d1e1bd62b426573ce929b332044fd
Turbobot <turbobot@vercel.com>
2023-12-04 14:35:27 -0300

    feat(create-turbo): apply official-starter transform

--- down.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-_() {
-  sudo docker-compose down
-} && _
-
-unset -f _
--- frontend/src/@core/domain/gateways/user.gateway.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "../entities/user";
-
-export interface UserGateway {
-    ping(): Promise<string>
-    singup(): Promise<boolean>
-    login(): Promise<User>
-}
