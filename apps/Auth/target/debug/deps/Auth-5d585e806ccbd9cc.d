54c48a12c67f32e1f8cde17ec98deac94aa81e33
fullzer4 <gabrielpelizaro@gmail.com>
2023-12-04 15:20:07 -0300

    new

---
+++ apps/Auth/target/debug/deps/Auth-5d585e806ccbd9cc.d
@@ -0,0 +1,5 @@
+/home/fullzer4/AcustticAI/apps/Auth/target/debug/deps/Auth-5d585e806ccbd9cc: src/main.rs
+
+/home/fullzer4/AcustticAI/apps/Auth/target/debug/deps/Auth-5d585e806ccbd9cc.d: src/main.rs
+
+src/main.rs:
---
+++ apps/Gateway/target/debug/deps/Gateway-a2ae7d34172678a0.d
@@ -0,0 +1,5 @@
+/home/fullzer4/AcustticAI/apps/Gateway/target/debug/deps/Gateway-a2ae7d34172678a0: src/main.rs
+
+/home/fullzer4/AcustticAI/apps/Gateway/target/debug/deps/Gateway-a2ae7d34172678a0.d: src/main.rs
+
+src/main.rs:
