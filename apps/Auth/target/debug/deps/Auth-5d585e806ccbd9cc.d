f641b8d977eb92ce7f1476b454dc3bcb50075525
fullzer4 <gabrielpelizzaro@gmail.com>
2023-12-18 16:49:35 -0300

    dev setup

--- apps/Auth/target/debug/deps/Auth-5d585e806ccbd9cc.d
+++
@@ -1,5 +0,0 @@
-/home/fullzer4/AcustticAI/apps/Auth/target/debug/deps/Auth-5d585e806ccbd9cc: src/main.rs
-
-/home/fullzer4/AcustticAI/apps/Auth/target/debug/deps/Auth-5d585e806ccbd9cc.d: src/main.rs
-
-src/main.rs:
--- apps/Gateway/target/debug/deps/Gateway-a2ae7d34172678a0.d
+++
@@ -1,5 +0,0 @@
-/home/fullzer4/AcustticAI/apps/Gateway/target/debug/deps/Gateway-a2ae7d34172678a0: src/main.rs
-
-/home/fullzer4/AcustticAI/apps/Gateway/target/debug/deps/Gateway-a2ae7d34172678a0.d: src/main.rs
-
-src/main.rs:
