8edd482d7e38d46f5b00d66ec1c3500a04903853
fullzer4 <gabrielpelizzaro@gmail.com>
2023-12-19 01:56:38 -0300

    python dev shell

--- flake.nix
+++ flake.nix
@@ -1,29 +1,33 @@
 {
+  description = "Nix flake for creating dev shells";
   inputs = {
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    systems,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (
-        system:
+  outputs =
+    { systems
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
           f nixpkgs.legacyPackages.${system}
-      );
-  in {
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.rustup
-          pkgs.nodejs
-          pkgs.yarn
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-        ];
-      };
-    });
-  };
+        );
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            rust-analyzer
+            nodejs
+            yarn
+          ];
+          packages = with pkgs; [ python311 virtualenv ] ++
+            (with pkgs.python311Packages; [ pip ]);
+        };
+      });
+    };
 }
