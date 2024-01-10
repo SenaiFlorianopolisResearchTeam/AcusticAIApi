4f5581eab3af7a48fe660fbc8193d4dd71a8a585
fullzer4 <gabrielpelizzaro@gmail.com>
2024-01-09 21:01:16 -0300

    new

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,20 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { SessionsModule } from './sessions/sessions.module';
+import { ModelsModule } from './models/models.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
+      host: '0.0.0.0',
+      port: 5432,
+      username: 'fullzer4',
+      password: '123456',
+      database: 'acustticai',
+      entities: [User],
       synchronize: true,
     }),
+    UsersModule,
+    SessionsModule,
+    ModelsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
