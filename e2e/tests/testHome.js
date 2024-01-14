4e4b44bc8e9db59ee3cfb17e2fbc9ee2d452dd5c
fullzer4 <garbrielpelizzaro@gmail.com>
2024-01-14 03:54:43 -0300

    more tests

---
+++ e2e/tests/testHome.js
@@ -0,0 +1,21 @@
+import { firefox } from "playwright";
+import cheerio from "cheerio";
+
+const testHome = async () => {
+    const browser = await firefox.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+  
+    const url = "127.0.0.1:3000";
+
+    try {
+        await page.goto(url);
+        console.info("INFO: Testing links... (Home)")
+    } catch (error) {
+        console.error("Error while scraping:", error);
+    } finally {
+        await browser.close();
+    }
+}
+
+export default testHome
