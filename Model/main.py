6524104d30d6eb07d3ca4b2450aeb8f70a164b82
fullzer4 <garbrielpelizzaro@gmail.com>
2024-01-14 04:34:11 -0300

    prom setup

--- Model/main.py
+++ Model/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from fastapi.testclient import TestClient
+from prometheus_fastapi_instrumentator import Instrumentator
 from server.user.router import router
 from server.models.hello import HelloResponse
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/", response_model=HelloResponse)
 async def root():
